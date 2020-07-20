80a85a902c987f58d6775b32c45168368b2642d3
(Benja Kunkel kunkelbenjamin08@gmail.com) <kunkelbenjamin08@gmail.com>
2020-07-20 13:16:56 -0300

    stupid coach

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,22 @@
+class QuestionsController < ApplicationController
+  
+  def ask
+  end
+  
+  def answer
+    @question = params[:question]
+    case @question
+      when ""
+        @answer = "String Vacio"
+      when "I am going to work"
+        @answer = "Great"
+      else
+        if @question.include?("?")
+            @answer = "Silly question, get dressed and go to work!"
+          else
+            @answer = "I don't care, get dressed and go to work!"
+        end
+    end
+  end
+
+end
